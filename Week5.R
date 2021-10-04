d6c029f1762c79dac52e97aa26a700db5f301fec
TENNC006 <85803070+TENNC006@users.noreply.github.com>
2021-10-03 22:02:47 -0400

    Add files via upload

---
+++ Week5.R
@@ -0,0 +1,50 @@
+library(readxl)
+library(dplyr)
+library(plyr)
+library(purrr)
+library(tidyverse)
+
+setwd("C:/Users/colli/Documents/GitHub/dsc520/data")
+
+
+housing_df <- read_xlsx("week-7-housing.xlsx")
+
+head(housing_df)
+
+##str(housing_df)
+
+##tail(housing_df)
+
+group_by(housing_df, sale_reason)
+
+housing_df %>% summarize(avg_sqft = mean(sq_ft_lot),
+                         med_sqft = median(sq_ft_lot),
+                         avg_sqft_liv = mean(square_feet_total_living),
+                         med_sqft_liv = median(square_feet_total_living))
+
+housing_df_mutate <- mutate(housing_df, p_pw = sq_ft_lot / max(sq_ft_lot) * 100)
+
+housing_df_filter <- filter(housing_df, sale_reason == 1)
+
+housing_df_select <- select(housing_df, -year_built)
+
+housing_df_arrange <- arrange(housing_df, desc(year_built))
+
+housing_df %>% map(is.numeric)
+
+housing_df %>% map_chr(is.numeric)
+``
+housing_df_cbind <- cbind(housing_df, housing_df_arrange)
+
+housing_df_rbind <- rbind(housing_df, housing_df_arrange)
+str(housing_df_rbind)
+
+
+str_cstr_split("hello my friend", pattern = " ")
+
+str_c("hello", "my", "friend", sep = " ")
+
+
+
+
+
